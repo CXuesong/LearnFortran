92f7664663a290357f702bb04c472d1eefff0851
forest93 <forest93@163.com>
2016-12-23 09:23:34 +0800

    Cleanup.

--- pi.f90
+++ pi.f90
@@ -1,20 +1,23 @@
 program piEvaluator
 
     use mpmodule
- 
+
     implicit none
-   
+
     ! c.f. https://en.wikipedia.org/wiki/Gauss%E2%80%93Legendre_algorithm
 
-    type(mp_real) :: a, b, t, p
+     type(mp_real) a, b, t, p
 
-    call mpinit()
+     call mpinit(10)
 
-    a = mpreal(1d0, 1)
-    b = sqrt(mpreal(0.5d0, 1))
-    t = mpreal(0.25d0, 1)
-    p = mpreal(1d0, 1)
+     !a = mpreal(1d0)
+     !b = sqrt(mpreal(0.5d0))
+     !t = mpreal(0.25d0)
+     !p = mpreal(1d0)
+     
+    
+    a = mppi()
 
-    print *, a
+    call mpwrite (6, 300, 100, a)
 
 end program piEvaluator
